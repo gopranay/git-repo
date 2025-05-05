fe8dc7f4d2ca105fcc80bcb0a689eaa8ef61b962
gopranay <gonepranay27@gmail.com>
2025-05-05 16:16:09 +0530

    added new class

---
+++ Collection/src/main/java/com/javapractise/Test.java
@@ -0,0 +1,10 @@
+package com.javapractise;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println(100 + 100 +"Capgemini");   
+  	             System.out.println("Capgemini" + 100 + 100);  
+
+	}
+
+}
